12652806ceb94c4d84cf0275809afe3c014e1eee
Kaiser-G <kaiserg6969@gmail.com>
2024-02-21 11:47:01 +0100

    mise en place exo 2

--- exercice01.js
+++ exercice01.js
@@ -1,6 +1,23 @@
 //pour l'exo 01
-alert("coucou");
+
+alert("Entrez votre nom");
+
+function verif(e) {
+  alert("Vérification de votre nom");
+  let longueur = $("#nom").val().length;
+
+  if (longueur < 2 || longueur > 5) {
+    alert("Le nom doit contenir entre 2 et 5 caractères");
+    $("#nom").css("background-color", "red");
+    e.preventDefault(); // annule le submit
+  } else {
+    alert("merci");
+    $("#nom").css("background-color", "lightgreen");
+    e.preventDefault(); // annule le submit
+  }
+}
+
 // Le programme PRINCIPAL
 $(document).ready(function () {
-  alert("La page est chargée");
+  $("#btn").click(verif);
 });
